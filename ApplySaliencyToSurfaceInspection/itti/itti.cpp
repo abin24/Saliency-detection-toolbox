9216367a187993f1d372fd3a2028333fa592643d
runagate <rotgarden@qq.com>
2022-09-23 10:25:40 +0800

    解决问题 无法将参数 从“cv::Size”转换为“CvSize”

--- ApplySaliencyToSurfaceInspection/itti/itti.cpp
+++ ApplySaliencyToSurfaceInspection/itti/itti.cpp
@@ -178,6 +178,8 @@ void ITTI::createOrientationFeatureMaps(int orientation)
 	int delta[2] = { 3, 4 };
 	CvGabor *gabor = new CvGabor(orientation, 0);
 	IplImage* gbr_fineScaleImage, *gbr_coarseScaleImage;
+	
+	CvSize sizeTemp;
 
 	for (int scaleIndex = 0; scaleIndex < fineScaleNumber; scaleIndex++)
 	{
@@ -188,7 +190,7 @@ void ITTI::createOrientationFeatureMaps(int orientation)
 		//IplImage src_fineScaleImage = cvarrToMat(fineScaleImage);
 		IplImage src_fineScaleImage = cvIplImage(fineScaleImage);
 
-		CvSize sizeTemp; sizeTemp.width = fineScaleImage.cols; sizeTemp.height = fineScaleImage.rows;
+		sizeTemp.width = fineScaleImage.cols; sizeTemp.height = fineScaleImage.rows;
 		//gbr_fineScaleImage = cvCreateImage(src_fineScaleImage..size(), IPL_DEPTH_8U, 1);
 		gbr_fineScaleImage = cvCreateImage(sizeTemp, IPL_DEPTH_8U, 1);
 
@@ -199,8 +201,13 @@ void ITTI::createOrientationFeatureMaps(int orientation)
 		{
 			int s = c[scaleIndex] + delta[scaleDiffIndex];
 			Mat coarseScaleImage = gaussianPyramid_I.at(s);
-			IplImage src_coarseScaleImage = IplImage(coarseScaleImage);
-			gbr_coarseScaleImage = cvCreateImage(coarseScaleImage.size(), IPL_DEPTH_8U, 1);
+			//IplImage src_coarseScaleImage = IplImage(coarseScaleImage);
+			IplImage src_coarseScaleImage = cvIplImage(coarseScaleImage);
+
+			//gbr_coarseScaleImage = cvCreateImage(coarseScaleImage.size(), IPL_DEPTH_8U, 1);
+			sizeTemp.width = coarseScaleImage.cols; sizeTemp.height = coarseScaleImage.rows;
+			gbr_coarseScaleImage = cvCreateImage(sizeTemp, IPL_DEPTH_8U, 1);
+
 			gabor->conv_img(&src_coarseScaleImage, gbr_coarseScaleImage, CV_GABOR_REAL);
 			Mat coarse_responseImg = cvarrToMat(gbr_coarseScaleImage);
 
